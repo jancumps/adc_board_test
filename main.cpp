6f84fdb91696b32a8691819dc87ffd697fb90586
Jan Cumps <jcumps@gmail.com>
2023-11-10 10:59:05 +0100

    allow to define i2c port with environment define

--- main.cpp
+++ main.cpp
@@ -31,7 +31,9 @@
 // set this value to 0 or 1 depending on 0-ohm resistor positions on ADC board.
 // set the value to 0 if the resistors are at the I2C0 labeled position,
 // set the value to 1 if the resistors are at the unlabeled position (which will be I2C1)
-#define I2C_PORT_SELECTED 1
+#ifndef I2C_PORT_SELECTED
+#define I2C_PORT_SELECTED 0
+#endif // I2C_PORT_SELECTED
 // these pins are supported by the ADC board:
 #define I2C_SDA0_PIN 4
 #define I2C_SCL0_PIN 5
